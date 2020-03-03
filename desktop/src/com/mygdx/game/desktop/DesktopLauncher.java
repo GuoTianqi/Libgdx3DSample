614a73219da09cf0c8ec3de51316fe491d4be907
Guo Tianqi <guotianqi1987@gmail.com>
2020-03-03 18:37:05 +0800

    add FrustumCullingTest

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,12 +2,13 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.FrustumCullingTest;
 import com.mygdx.game.LoadSceneTest;
 import com.mygdx.game.MyGdxGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new LoadSceneTest(), config);
+		new LwjglApplication(new FrustumCullingTest(), config);
 	}
 }
